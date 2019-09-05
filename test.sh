9ecaa4aa9c7b15b08b2044c92dd7cda31d699d69
beet-aizu <aki.bdash@gmail.com>
2019-09-05 10:50:49 +0900

    resolve infinite loop

--- test.sh
+++ test.sh
@@ -1,22 +1,16 @@
 #!/bin/bash
-
 git config --global user.name "beet-aizu"
 git config --global user.email "aki.bdash@gmail.com"
 
 git remote set-url origin https://beet-aizu:${GITHUB_TOKEN}@github.com/beet-aizu/github_actions.git
 
-git log -1 | head -1 > last_commit.txt
+last_commit_message ="$(git log -1 | tail -1)"
+echo last_commit_message
+echo last_commit_message | grep [updater]
 
-cat last_commit.txt
-
-git add last_commit.txt
-
-git commit -m 'update last commit'
-
-git log
-
-git status
-
-git branch
-
-git push
+if [ -z "$(echo last_commit_message | grep [updater])" ]; then
+    git log -1 | head -1 > last_commit.txt
+    git add last_commit.txt
+    git commit -m '[updater] update last commit'
+    git push origin HEAD
+fi
