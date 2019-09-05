d89ee5d9f101bcd97ac08690eff15aee04fd5d7c
beet-aizu <aki.bdash@gmail.com>
2019-09-05 10:41:37 +0900

    add debug info

--- test.sh
+++ test.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-git config --global user.name "beet"
-git config --global user.email "you@example.com"
+git config --global user.name "updater"
+git config --global user.email "updater@example.com"
 
 git remote set-url origin https://beet-aizu:${GITHUB_TOKEN}@github.com/beet-aizu/github_actions.git
 
 git log -1 | head -1 > last_commit.txt
 
+cat last_commit.txt
+
 git add last_commit.txt
 
 git commit -m 'update last commit'
+
+git log
+
+git status
+
+git branch
 
 git push origin master
