3cc30ae341713ff976cafc5106cde74d1ad4b0b2
beet-aizu <aki.bdash@gmail.com>
2019-09-05 10:51:56 +0900

    fix

--- test.sh
+++ test.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
+
 git config --global user.name "beet-aizu"
 git config --global user.email "aki.bdash@gmail.com"
 
 git remote set-url origin https://beet-aizu:${GITHUB_TOKEN}@github.com/beet-aizu/github_actions.git
 
-last_commit_message ="$(git log -1 | tail -1)"
-echo last_commit_message
-echo last_commit_message | grep [updater]
+last_commit_message="$(git log -1 | tail -1)"
+echo $last_commit_message
+echo $last_commit_message | grep [updater]
 
-if [ -z "$(echo last_commit_message | grep [updater])" ]; then
+if [ -z "$(echo $last_commit_message | grep [updater])" ]; then
     git log -1 | head -1 > last_commit.txt
     git add last_commit.txt
     git commit -m '[updater] update last commit'
